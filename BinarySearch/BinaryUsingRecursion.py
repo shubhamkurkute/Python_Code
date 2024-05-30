5a7faebc1b0b9b1df54264a1df6f7f37cbd3f206
shubhcctech <shubham.kurkute@cctech.co.in>
2024-05-30 20:51:46 +0530

    Binary with Recursion

--- BinarySearch/BinaryUsingRecursion.py
+++ BinarySearch/BinaryUsingRecursion.py
@@ -1,8 +1,24 @@
-def BinaryRecursion(element):
-    find = 8
-    sorted = [2,3,4,7,8,9]
-    start = 0
-    end = len(sorted)-1
+def BinaryRecursion(start,end,array):
+    find = 2
+    
+    mid = (int)((start+end)/2)
+    if(start<=end):
+        if(array[mid]>find):
+            BinaryRecursion(0,mid-1,array)
+        elif(array[mid]<find):
+            BinaryRecursion(mid+1,end,array)
+        else:
+            print(mid)
+    else:
+        print(-1)
+    
+
+
+sorted = [2,3,4,7,8,9]
+start = 0
+end = len(sorted)-1
+BinaryRecursion(start,end,sorted)
+
     
      
 
