40a1edceef39c56c6c7a039ea89b0726fe1cb382
shubhcctech <shubham.kurkute@cctech.co.in>
2024-07-23 22:15:38 +0530

    LinkedList

--- Datastructures/LinkedList/LinkedListInfo.py
+++ Datastructures/LinkedList/LinkedListInfo.py
@@ -9,3 +9,47 @@ class Node:
     def __init__(self,data):
         self.data = data
         self.next = None
+
+    
+
+class LinkedList:
+    def __init__(self):
+        self.head = None  # Initialize head as None
+
+    def insertAtBeginning(self, new_data):
+        new_node = Node(new_data)  # Create a new node 
+        new_node.next = self.head  # Next for new node becomes the   current head
+        self.head = new_node  # Head now points to the new node
+
+    def insertAtEnd(self, new_data):
+        new_node = Node(new_data)  # Create a new node
+        if self.head is None:
+            self.head = new_node  # If the list is empty, make the new node the head
+            return
+        last = self.head 
+        while last.next:  # Otherwise, traverse the list to find the last node
+            last = last.next
+        last.next = new_node  # Make the new node the next node of the last node
+
+    def printList(self):
+        temp = self.head # Start from the head of the list
+        while temp:
+            print(temp.data,end=' ') # Print the data in the current node
+            temp = temp.next # Move to the next node
+        print()  # Ensures the output is followed by a new line
+
+if __name__ == '__main__':
+    # Create a new LinkedList instance
+        llist = LinkedList()
+
+        # Insert each letter at the beginning using the method we created
+        llist.insertAtBeginning('fox') 
+        llist.insertAtBeginning('brown') 
+        llist.insertAtBeginning('quick')  
+        llist.insertAtBeginning('the')  
+        llist.insertAtEnd("is mad")
+
+    # Now 'the' is the head of the list, followed by 'quick', then 'brown' and 'fox'
+
+    # Print the list
+        llist.printList()
